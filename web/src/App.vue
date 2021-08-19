f4c07f31832d5669a16191360bf769b6166ce6f7
weizi <1952253119@qq.com>
2021-08-20 01:24:21 +0800

    完成公共组件header、footer开发

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <a-layout>
-      <a-layout-header class="header">
-        <div class="logo" />
-        <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-        >
-          <a-menu-item key="1">nav 1</a-menu-item>
-          <a-menu-item key="2">nav 2</a-menu-item>
-          <a-menu-item key="3">nav 3</a-menu-item>
-        </a-menu>
-      </a-layout-header>
+      <the-header></the-header>
       <router-view/>
-      <a-layout-footer style="text-align: center">
-            Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
+      <the-footer></the-footer>
     </a-layout>
 
 </template>
+<!--引入the-header-->
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import TheHeader from '@/components/the-header.vue';
+    import TheFooter from '@/components/the-footer.vue';
+    // import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+    export default defineComponent({
+        name: 'app',
+        components: {
+            TheHeader,
+            TheFooter
+        },
+    });
+</script>
+
+
 
 <style>
 #app {
