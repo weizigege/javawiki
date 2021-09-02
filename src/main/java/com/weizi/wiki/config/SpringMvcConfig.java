85b0ede55051e8b73e14a72a40d1386106b269e8
weizi <1952253119@qq.com>
2021-09-03 02:10:38 +0800

    新增aop打印接口详细参数耗时，去掉过滤器，拦截器

--- src/main/java/com/weizi/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/weizi/wiki/config/SpringMvcConfig.java
@@ -1,35 +1,35 @@
-package com.weizi.wiki.config;
-
-
-import com.weizi.wiki.intercepter.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor loginInterceptor;
-
-
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**"
-                );
-
-
-    }
-}
+//package com.weizi.wiki.config;
+//
+//
+//import com.weizi.wiki.intercepter.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/test/**",
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**"
+//                );
+//
+//
+//    }
+//}
