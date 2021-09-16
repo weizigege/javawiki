014bbb93299c12b41bbce04c62c7e6e0f4e40af8
weizi <1952253119@qq.com>
2021-09-17 00:01:45 +0800

    编辑时修改复制表单对象，修改时在界面上不会实时刷新

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -90,6 +90,7 @@
     import {defineComponent, onMounted, ref} from 'vue';
     import axios from 'axios';
     import {message} from 'ant-design-vue';
+    import {Tool} from '@/util/tool'
 
     export default defineComponent({
         name: 'AdminEbook',
@@ -125,7 +126,7 @@
             };
             const edit = (record: any) => {
                 modalVisible.value = true;
-                ebook.value = record;
+                ebook.value = Tool.copy(record);
             }
 
             const add = () => {
@@ -251,6 +252,7 @@
                 modalLoading,
                 handleModalOk,
                 ebook,
+                handleQuery
 
             }
         }
